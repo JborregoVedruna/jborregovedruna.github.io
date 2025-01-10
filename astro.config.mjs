aa40b3acfa560cb07bc530e1df51dad30607e12a
Joaquin <joaquin.borrego@vedruna.es>
2025-01-10 16:59:08 +0100

    joseca

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://jborregovedruna.github.io',
-    base: 'my-repo',
+    base: '/jborregovedruna.github.io',
 });
